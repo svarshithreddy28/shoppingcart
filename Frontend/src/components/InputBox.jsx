db4d280d7857b4861f3ef08a087b899cbf5be880
varshith <sarasanivarshith@gmail.com>
2024-08-15 09:07:57 +0530

    changes done

---
+++ Frontend/src/components/InputBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputBox = () => {
+  return (
+    <div>
+      <input type="text" name="" id="" />
+      <button>alert</button>
+    </div>
+  )
+}
+
+export default InputBox
